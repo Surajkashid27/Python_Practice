4c1ee03c24bb9660535e4e1bce7bed30d3011f18
Suraj Kashid <kashidsuraj1999@gmail.com>
2026-08-20 15:52:55 -0400

    Initial Commit

---
+++ pythontest.py
@@ -0,0 +1,31 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "initial_id",
+   "metadata": {
+    "collapsed": true
+   },
+   "outputs": [],
+   "source": [
+    "# 1. Print Function\n",
+    "\n",
+    "#Program 1: Print Your Details\n",
+    "print(\"Name: John\")\n",
+    "print(\"Age: 22\")\n",
+    "print(\"City: Delhi\")\n",
+    "\n",
+    "#Program 2: Print a Pattern\n",
+    "print(\"*****\")\n",
+    "print(\"*****\")\n",
+    "print(\"*****\")\n",
+    "\n",
+    "#Program 3: Welcome Message\n",
+    "print(\"Welcome to Python Programming!\")\n",
+    "print(\"Today is a great day to learn Python.\")\n",
+    "\n",
+    "#Program 4: Favorite Quote\n",
+    "print(\"Success comes from consistent practice.\")"
+   ]
+  }
